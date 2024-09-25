7a69a4f7a62cefabc3ed9e61375f0464a967246e
adeikalamalbert <31449810+adeikalamalbert@users.noreply.github.com>
2024-09-25 14:34:31 +0200

    ex05.c

---
+++ ex05.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		ft_putchar(*str++);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+		ft_putstr("-2147483648");
+	else
+	{
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			nb = -nb;
+		}
+		if (nb > 9)
+			ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	while (par->str)
+	{
+		ft_putstr(par->str);
+		ft_putchar('\n');
+		ft_putnbr(par->size);
+		ft_putchar('\n');
+		ft_putstr(par->copy);
+		ft_putchar('\n');
+		par++;
+	}
+}
